4d57aaeaa224f4e5141a9cb5725fb080370dc00e
Jair Prada <jairprada@gmail.com>
2026-08-29 13:12:33 -0500

    chore: externalize Firebase config to REACT_APP_FIREBASE_* env vars
    Move hardcoded Firebase web config to environment variables (already
    created in Vercel project env). Add .env.example template.

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-var firebaseConfig = {
-    apiKey: "AIzaSyCZ29Gg4qfuWCZ74eyx5yR3JXCSc-TwNjo",
-    authDomain: "heroes-app-91da8.firebaseapp.com",
-    projectId: "heroes-app-91da8",
-    storageBucket: "heroes-app-91da8.appspot.com",
-    messagingSenderId: "854285506539",
-    appId: "1:854285506539:web:105dcea4ff762b4ad0ebae",
-    measurementId: "G-SE4ECW8RP9"
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-firebase.initializeApp(firebaseConfig)
+
+firebase.initializeApp(firebaseConfig);
 export default firebase;
